b2139fa9287268be4483d2d943640d26be6d4c1d
jtomasenti <jordi.tomas@enti.cat>
2020-11-16 10:01:09 +0100

    Clase de condiciones

---
+++ condiciones.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "COMPROBADOR DE EXISTENCIA DE ARCHIVOS v1.0"
+echo "------------------------------------------"
+echo ""
+echo "Indica el nombre de archivo a comprobar:"
+
+read ARCHIVO
+
+if [ -e	$ARCHIVO ]; then
+	cowsay "El archivo existe"
+else
+	echo "El archivo no existe"		
+fi
---
+++ filtrado.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Elemento de filtrado"
